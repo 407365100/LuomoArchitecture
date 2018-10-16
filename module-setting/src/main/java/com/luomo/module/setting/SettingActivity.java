04fd9146473dbadce37a84c668f87fb0e42d35a0
renpan <pan.ren@ezhantu.com>
2018-10-16 15:01:25 +0800

    概要： 提交内容： 无 提交说明：无

---
+++ module-setting/src/main/java/com/luomo/module/setting/SettingActivity.java
@@ -0,0 +1,13 @@
+package com.luomo.module.setting;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_setting);
+    }
+}
